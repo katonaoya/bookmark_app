64c28338c6605f077f9956662a4f07732f51c3da
katonaoya <kato7080220@yahoo.co.jp>
2021-11-03 13:56:45 +0900

    db:migrate

--- db/schema.rb
+++ db/schema.rb
@@ -10,22 +10,24 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20_211_024_070_058) do
-  create_table 'mybooks', force: :cascade do |t|
-    t.string 'title', null: false
-    t.string 'memo'
-    t.integer 'page', null: false
-    t.text 'feedback'
-    t.datetime 'created_at', null: false
-    t.datetime 'updated_at', null: false
+ActiveRecord::Schema.define(version: 2021_10_24_070058) do
+
+  create_table "mybooks", force: :cascade do |t|
+    t.string "title", null: false
+    t.string "memo"
+    t.integer "page", null: false
+    t.text "feedback"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
-  create_table 'records', force: :cascade do |t|
-    t.integer 'readed_page', null: false
-    t.text 'comment'
-    t.integer 'mybook_id'
-    t.datetime 'created_at', null: false
-    t.datetime 'updated_at', null: false
-    t.index ['mybook_id'], name: 'index_records_on_mybook_id'
+  create_table "records", force: :cascade do |t|
+    t.integer "readed_page", null: false
+    t.text "comment"
+    t.integer "mybook_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["mybook_id"], name: "index_records_on_mybook_id"
   end
+
 end
